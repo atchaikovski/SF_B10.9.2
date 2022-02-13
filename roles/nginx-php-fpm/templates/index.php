cdb111ad81cefb090202fb5a6c0b651910324ddb
Alex Tchaikovski <alex@tchaikovski.net>
2022-02-13 11:42:00 +0300

    nginx-php-fpm

---
+++ roles/nginx-php-fpm/templates/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>main index file B10.9.2</title>
+<style>
+    html { color-scheme: light dark; }
+    body { width: 35em; margin: 0 auto;
+    font-family: Tahoma, Verdana, Arial, sans-serif; }
+</style>
+</head>
+<body>
+    <H2>index.html and nginx.conf test with php-fpm</H2>
+    <?php phpinfo();?>
+
+</body>
+</html>
+
